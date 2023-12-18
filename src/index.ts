ec6ca1b4023db08eea5e2dc930e05bf01295f90e
mariobob <35691890+Mariobob@users.noreply.github.com>
2023-12-17 23:26:00 -0500

    Remove police attack check and add mission completion check

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,7 @@ function sendNotification(device, title: String, body: String) {
     pusher.note(device, title, body)
 }
 
+let activeMissions: number = 0;
 console.log("Running...");
 const JournalOptions: JournalOptions = {
     watch: true,
@@ -30,11 +31,11 @@ try {
         } else if (event.event === "FighterDestroyed") {
             sendNotification({}, "Fighter Destroyed", "Fighter destroyed, Commander!")
             console.log(event.timestamp + ": Fighter destroyed, Commander!")
-        } else if (event.event === "ReceiveText") {
-            if (event.From_Localised === "System Authority Vessel" && (event.Message.includes("Police_Attack") || event.Message.includes("OverwatchAttackRun"))) { //Added due to a bug in ED where police might attack you for no reason. Can be removed if fixed.
-                console.log("Police attack detected, Commander!")
-                sendNotification({}, "Police Attack", "Police attack detected, Commander!");
-            }
+        // } else if (event.event === "ReceiveText") {
+        //     if (event.From_Localised === "System Authority Vessel" && (event.Message.includes("Police_Attack") || event.Message.includes("OverwatchAttackRun"))) { //Added due to a bug in ED where police might attack you for no reason. Can be removed if fixed.
+        //         console.log("Police attack detected, Commander!")
+        //         sendNotification({}, "Police Attack", "Police attack detected, Commander!");
+        //     }
         } else if (event.event === "CollectCargo") {
             if (event.Stolen) {
                 console.log("Stolen cargo collected, Commander!")
@@ -43,6 +44,27 @@ try {
         } else if (event.event === "Died") {
             console.log("Commander has died!")
             sendNotification({}, "Commander has died!", "Commander has died!");
+        } else if (event.event === "Missions") {
+            let tempMissions = 0;
+            for (let index = 0; index < event.Active.length; index++) {
+                const element = event.Active[index];
+                if (element.Expires > 0) {
+                    tempMissions++;
+                }
+            }
+            activeMissions = tempMissions;
+            console.log("Active missions: " + activeMissions)
+        } else if (event.event === "MissionRedirected" || event.event === "MissionFailed" || event.event === "MissionAbandoned") {
+            activeMissions--;
+            if (activeMissions === 0) {
+                console.log("All missions completed, Commander!")
+                sendNotification({}, "All Missions Completed", "All missions completed, Commander!");
+            } else {
+                console.log(activeMissions + " missions remaining, Commander!")
+            }
+        } else if (event.event === "MissionAccepted") {
+            activeMissions++;
+            console.log(activeMissions + " active missions, Commander!")
         }
     };
 } catch (err) {
